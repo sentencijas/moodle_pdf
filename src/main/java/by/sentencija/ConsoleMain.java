9fb6589b029b55ab8df6c4bbcf6df0e92dcd5efd
Max Matveenko <supmaks8653@gmail.com>
2025-06-19 14:08:19 +0300

    add help text to the console version

--- src/main/java/by/sentencija/ConsoleMain.java
+++ src/main/java/by/sentencija/ConsoleMain.java
@@ -1,14 +1,25 @@
 package by.sentencija;
 
 import by.sentencija.ui.UI;
+import lombok.val;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.awt.image.ConvolveOp;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class ConsoleMain {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        UI.parse("./src/main/resources/b.mbz", "src/main/resources", false);
+        if(args.length == 0){
+            System.out.println("""
+                    Usage: provide 2 arguments which are:
+                    1.Path to the backup .mbz file;
+                    2.Path to the folder to save the result PDF file to. If this is not provided, the file will be save to the same directory as the source file.
+                    """);
+        }
+        val source = args[0];
+        val target = args.length == 2 ? args[1] : Path.of(source).getParent().toString();
+        UI.parse(source, target, true);
     }
-    //TODO добавить тест с картинкой из теста 7 или итогового теста
 }
